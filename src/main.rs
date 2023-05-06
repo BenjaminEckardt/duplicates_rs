70a7817d7e406b6da6a9e1c157afe773a9315970
Benjamin Eckardt <benjamin.eckardt@beneck.de>
2023-05-07 00:55:11 +0200

    Updates project to latest edition, replaces MD5 lib with more sophisticated and applies minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,47 @@
-extern crate crypto;
-extern crate glob;
-
-use crypto::md5::Md5;
-use crypto::digest::Digest;
+use md5::{Md5, Digest};
 use glob::glob;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
+
+fn hash_content(reader: &mut dyn Read) -> String {
+    let digest = {
+        let mut hasher = Md5::new();
+        let mut buffer = vec![0; 1024 * 1024];
+        loop {
+            let count = reader.read(&mut buffer).unwrap();
+            if count == 0 { break }
+            hasher.update(&buffer[..count]);
+        }
+        hasher.finalize()
+    };
+    base16ct::lower::encode_string(&digest)
+}
 
 fn main() {
     let pattern = env::args().nth(1).unwrap_or("./**/*".to_string());
 
-    let results: HashMap<String, Vec<String>> = glob(&pattern).unwrap().into_iter()
+    glob(&pattern).unwrap().into_iter()
         .map(|path_result| path_result.unwrap())
         .filter(|path| !path.is_dir())
         .map(|file_path| {
             let mut file = File::open(file_path.clone()).unwrap();
-            let mut buffer = Vec::new();
-            file.read_to_end(&mut buffer).unwrap();
-
-            let mut hasher = Md5::new();
-            hasher.input(buffer.as_slice());
-            let hash = hasher.result_str();
-
-            (hash, file_path.to_str().unwrap().to_string())
-        }).fold(HashMap::new(), |mut result_map, hash_to_path| {
-            if !result_map.contains_key(&hash_to_path.0) {
-                result_map.insert(hash_to_path.0, vec![hash_to_path.1]);
-            } else {
-                result_map.get_mut(&hash_to_path.0).unwrap().push(hash_to_path.1);
-            }
+            let hex_hash = hash_content(&mut file);
+            (hex_hash, file_path.to_str().unwrap().to_string())
+        })
+        .fold(HashMap::new(), |mut result_map, (hash, path)| {
+            let paths_for_hash = result_map.entry(hash).or_insert(vec![]);
+            paths_for_hash.push(path);
             result_map
-        });
-
-    for (hash, paths) in results.iter() {
-        if paths.len() > 1 {
+        })
+        .into_iter()
+        .filter(|(_hash, paths)| paths.len() > 1)
+        .for_each(|(hash, paths)| {
             println!("Found {} occurrences:", paths.len());
             for p in paths {
                 println!("Path: {}", p);
             }
-            println!("MD5-Hash: {}\n", hash);
-        }
-    }
+            println!("Sha256 Hash: {}\n", hash);
+        });
 }
